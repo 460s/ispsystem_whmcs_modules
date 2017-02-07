33c97e50153a5b737d55aeaab351e4265b3582ff
D Syrovatskiy <d.syrovatskiy@ispsystem.com>
2017-02-07 20:38:57 +0800

    replacement deprecated functions

--- addons/ispsystem_partners/ispsystem_partners.php
+++ addons/ispsystem_partners/ispsystem_partners.php
@@ -13,6 +13,8 @@
 if (!defined("WHMCS"))
     die("This file cannot be accessed directly");
 
+use WHMCS\Database\Capsule as DB;
+
 function ispsystem_partners_config() {
 	$configarray = array(
 		"name" => "ISPsystem NOC Partner module",
@@ -63,116 +65,111 @@ function ispsystem_partners_create_packages($order,$name,$desc,$price,$pgid,$ser
 	);
 
 	// Найдем id тарифа, который создали
-	$result = select_query('tblproducts','id','','id','DESC','1');
-	$data = mysql_fetch_array($result);
-	$package_id = $data['id'];
+    $result = DB::table('tblproducts')->select('id')->orderBy('id', 'desc')->first();
+    $package_id = $result->id;
 
 	// Добавим опцию ip
-	insert_query ('tblcustomfields',array(
-		'type' => 'product'
-		, 'relid' => $package_id
-		, 'fieldname' => 'ip'
-		, 'description' => 'IP address for the license'
-		, 'regexpr' => '/^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/'
-		, 'required' => 'on'
-		, 'showorder' => 'on'
-		, 'fieldtype' => 'text'
-	));
+    DB::table('tblcustomfields')->insert([
+        'type' => 'product',
+        'relid' => $package_id,
+        'fieldname' => 'ip',
+        'description' => 'IP address for the license',
+        'regexpr' => '/^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/',
+        'required' => 'on',
+        'showorder' => 'on',
+        'fieldtype' => 'text'
+    ]);
 
 	// Добавим опцию name
-	insert_query ('tblcustomfields',
-		array('type' => 'product'
-		, 'relid' => $package_id
-		, 'fieldname' => 'name'
-		, 'regexpr' => '/.+/'
-		, 'description' => 'License name'
-		, 'required' => 'on'
-		, 'showorder' => 'on'
-		, 'fieldtype' => 'text'
-		)
-	);
+    DB::table('tblcustomfields')->insert([
+        'type' => 'product',
+		'relid' => $package_id,
+		'fieldname' => 'name',
+		'regexpr' => '/.+/',
+		'description' => 'License name',
+		'required' => 'on',
+		'showorder' => 'on',
+		'fieldtype' => 'text'
+    ]);
 
 	// Добавим опцию lickey
-	insert_query ('tblcustomfields',
-		array('type' => 'product'
-		, 'relid' => $package_id
-		, 'fieldname' => 'lickey'
-		, 'description' => 'License key'
-		, 'fieldtype' => 'text'
-		)
-	);
+    DB::table('tblcustomfields')->insert([
+        'type' => 'product',
+		'relid' => $package_id,
+		'fieldname' => 'lickey',
+		'description' => 'License key',
+		'fieldtype' => 'text'
+    ]);
 
 	// Создадим группы опций и опции для дозаказа дополнений
 	if ($addon_id != '') {
 
-		$gid = insert_query('tblproductconfiggroups', array('name' => $name . ' options group'));
-		$option_id = insert_query('tblproductconfigoptions',
-			array('gid' => $gid
-			, 'optionname' => 'Additional nodes in cluster'
-			, 'optiontype' => '4'
-			, 'qtyminimum' => 0
-			, 'qtymaximum' => 255
-			)
-		);
+        $gid = DB::table('tblproductconfiggroups')->insertGetId(['name' => $name . ' options group']);
 
-		$option_sub_id = insert_query('tblproductconfigoptionssub', array('configid' => $option_id, 'optionname' => 'servers'));
-		insert_query('tblpricing',
-			array('type' => 'configoptions'
-			, 'currency' => 1
-			, 'relid' => $option_sub_id
-			, 'msetupfee' => '0.00'
-			, 'qsetupfee' => '0.00'
-			, 'ssetupfee' => '0.00'
-			, 'asetupfee' => '0.00'
-			, 'bsetupfee' => '0.00'
-			, 'tsetupfee' => '0.00'
-			, 'quarterly' => '0.00'
-			, 'semiannually' => '0.00'
-			, 'annually' => '0.00'
-			, 'biennially' => '0.00'
-			, 'triennially' => '0.00'
-			, 'monthly' => $addon_price
-			)
-		);
-		insert_query ('tblproductconfiglinks',	array('gid' => $gid, 'pid' => $package_id) );
+        $option_id = DB::table('tblproductconfigoptions')->insertGetId([
+            'gid' => $gid,
+			'optionname' => 'Additional nodes in cluster',
+			'optiontype' => '4',
+			'qtyminimum' => 0,
+			'qtymaximum' => 255
+         ]);
+
+        $option_sub_id = DB::table('tblproductconfigoptionssub')->insertGetId(['configid' => $option_id, 'optionname' => 'servers']);
+
+        DB::table('tblpricing')->insert([
+            'type' => 'configoptions',
+            'currency' => 1,
+            'relid' => $option_sub_id,
+            'msetupfee' => '0.00',
+            'qsetupfee' => '0.00',
+            'ssetupfee' => '0.00',
+            'asetupfee' => '0.00',
+            'bsetupfee' => '0.00',
+            'tsetupfee' => '0.00',
+            'quarterly' => '0.00',
+            'semiannually' => '0.00',
+            'annually' => '0.00',
+            'biennially' => '0.00',
+            'triennially' => '0.00',
+            'monthly' => $addon_price
+        ]);
+
+        DB::table('tblproductconfiglinks')->insert(['gid' => $gid, 'pid' => $package_id]);
 	}
 
 	// Создадим аддоны для остальных тарифов
-	$new_addon_id = insert_query ('tbladdons',
-		array('name' => $name
-		, 'description' => $desc
-		, 'billingcycle' => 'Monthly'
-		, 'showorder' => 'on'
-		, 'autoactivate' => 'on'
-		)
-	);
+    $new_addon_id = DB::table('tbladdons')->insertGetId([
+        'name' => $name,
+		'description' => $desc,
+		'billingcycle' => 'Monthly',
+		'showorder' => 'on',
+		'autoactivate' => 'on'
+     ]);
 	// Выставляем цену аддона
-	insert_query('tblpricing',
-		array('type' => 'addon'
-		, 'currency' => 1
-		, 'relid' => $new_addon_id
-		, 'msetupfee' => '0.00'
-		, 'qsetupfee' => '0.00'
-		, 'ssetupfee' => '0.00'
-		, 'asetupfee' => '0.00'
-		, 'bsetupfee' => '0.00'
-		, 'tsetupfee' => '0.00'
-		, 'quarterly' => '0.00'
-		, 'semiannually' => '0.00'
-		, 'annually' => '0.00'
-		, 'biennially' => '0.00'
-		, 'triennially' => '0.00'
-		, 'monthly' => $price
-		)
-	);
+    DB::table('tblpricing')->insert([
+        'type' => 'addon',
+		'currency' => 1,
+		'relid' => $new_addon_id,
+		'msetupfee' => '0.00',
+		'qsetupfee' => '0.00',
+		'ssetupfee' => '0.00',
+		'asetupfee' => '0.00',
+		'bsetupfee' => '0.00',
+		'tsetupfee' => '0.00',
+		'quarterly' => '0.00',
+		'semiannually' => '0.00',
+		'annually' => '0.00',
+		'biennially' => '0.00',
+		'triennially' => '0.00',
+		'monthly' => $price
+     ]);
 	// Запомним id аддона, чтобы потом обрабатывать его автоматически
-	insert_query('ispsystem_noc_addon',
-		array('addonid' => $new_addon_id
-			, 'priceid' => $price_id
-			, 'serverid' => $server_id
-			, 'priceaddon' => $addon_id
-		)
-	);
+    DB::table('ispsystem_noc_addon')->insert([
+        'addonid' => $new_addon_id,
+		'priceid' => $price_id,
+		'serverid' => $server_id,
+		'priceaddon' => $addon_id
+     ]);
 
 }
 
@@ -189,7 +186,7 @@ function ispsystem_partners_activate() {
 		, serviceaddon INT(11) DEFAULT NULL
 		, PRIMARY KEY (id))
 		ENGINE=InnoDB DEFAULT CHARSET=utf8";
-	$result = mysql_query($query);
+	$result = full_query($query);
 
 	// Создаем базу для хранения id аддонов
 	$query = "CREATE TABLE ispsystem_noc_addon(
@@ -200,29 +197,29 @@ function ispsystem_partners_activate() {
 		, serverid INT(11) NOT NULL
 		, PRIMARY KEY (id))
 		ENGINE=InnoDB DEFAULT CHARSET=utf8";
-	$result = mysql_query($query);
+	$result = full_query($query);
 
 	// Создаем группу серверов
-	$server_group_id = insert_query('tblservergroups', array('name' => 'BILLmanager NOC', 'filltype' => 1));
+    $server_group_id = DB::table('tblservergroups')->insertGetId(['name' => 'BILLmanager NOC', 'filltype' => 1]);
 
 	// Создаем сервер
-	$server_id = insert_query('tblservers',array(
-		'name' => 'ISPsystem NOC'
-		, 'hostname' => 'https://api.ispsystem.com/manager/billmgr'
-		, 'secure' => 'on'
-		, 'type' => 'billmanager_noc'
-		, 'active' => 1
-		, 'disabled' => 0
-	));
+    $server_id  = DB::table('tblservers')->insertGetId([
+        'name' => 'ISPsystem NOC',
+        'hostname' => 'https://api.ispsystem.com/manager/billmgr',
+		'secure' => 'on',
+		'type' => 'billmanager_noc',
+		'active' => 1,
+		'disabled' => 0
+     ]);
 
 	// Привяжем сервер к группе
-	insert_query('tblservergroupsrel',array('groupid' => $server_group_id, 'serverid' => $server_id));
+    DB::table('tblservergroupsrel')->insert(['groupid' => $server_group_id, 'serverid' => $server_id]);
 
 	// Создаем группу тарифов
-	$product_group_id = insert_query ('tblproductgroups',array(
-		'name' => 'ISPsystem Software'
-		,'orderfrmtpl' => 'comparison'
-	));
+    $product_group_id  = DB::table('tblproductgroups')->insertGetId([
+        'name' => 'ISPsystem Software',
+		'orderfrmtpl' => 'comparison'
+     ]);
 
 	// Создаем тарифы
 	ispsystem_partners_create_packages(1,'ISPmanager 5 Lite','Control panel for managing your personal server','4.00',$product_group_id,$server_id,$server_group_id,'3541');
@@ -235,6 +232,7 @@ function ispsystem_partners_activate() {
 }
 
 function ispsystem_partners_deactivate() {
+    #Возвращаем результат
 	return array('status'=>'success','description'=>'Module deactivated. Please delete packages\addons\servers manually');
 }
 
