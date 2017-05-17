b50a787561cf22455cf5c77e1440aa2aac675c0f
D Syrovatskiy <d.syrovatskiy@ispsystem.com>
2017-05-17 11:02:29 +0800

    fix typo

--- servers/dcimanager/lib/server.php
+++ servers/dcimanager/lib/server.php
@@ -12,7 +12,7 @@ class Server
         $this->username = $params["serverusername"];
         $this->password = $params["serverpassword"];
     }
-    
+
     public function apiRequest($func, $param = []) {
 	global $op;
 
@@ -30,7 +30,7 @@ class Server
 	logModuleCall("dcimanager:".$func, $op, array_merge($postfields, $param), $response, $response, array ($this->password));
 
 	try {
-		$out = new \SimpleXMLElement($response);
+		$out = new SimpleXMLElement($response);
 	} catch (Exception $e) {
 		$out = simplexml_load_string($default_xml_error_string);
 		$out->error->addChild("msg", $e->getMessage());
